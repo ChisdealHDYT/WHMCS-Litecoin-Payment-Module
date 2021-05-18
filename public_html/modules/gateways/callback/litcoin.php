a2857299c681bbda6120caa224e1686281bc5e7b
Grant Robinson <Chisdeal2014@hotmail.com>
2021-05-18 11:22:37 +0100

    Update litcoin.php

--- public_html/modules/gateways/callback/litcoin.php
+++ public_html/modules/gateways/callback/litcoin.php
@@ -6,7 +6,7 @@ include("../../../includes/functions.php");
 include("../../../includes/gatewayfunctions.php");
 include("../../../includes/invoicefunctions.php");
 
-$gatewaymodule = "litecoin"; # Enter your gateway module name here replacing template
+$gatewaymodule = "zenzo"; # Enter your gateway module name here replacing template
 
 $GATEWAY = getGatewayVariables($gatewaymodule);
 if (!$GATEWAY["type"]) die("Module Not Activated"); # Checks gateway module is active before accepting callback
@@ -16,9 +16,9 @@ if (!$GATEWAY["type"]) die("Module Not Activated"); # Checks gateway module is a
 	$h = $GATEWAY['host'].':'.$GATEWAY['port'];
 	$rpc = 'http://'.$u.':'.$p.'@'.$h;
 	# Build Litecoin Information Here
-	require_once '../litcoin/jsonRPCClient.php';
-	$litecoin = new jsonRPCClient($rpc); 
-	if(!$litecoin->getinfo()){
+	require_once '../zenzo/jsonRPCClient.php';
+	$zenzo = new jsonRPCClient($rpc); 
+	if(!$zenzo->getinfo()){
 		die('could not connect to litcoind');
 	}
 	
@@ -27,12 +27,12 @@ $results = mysql_query($sql);
 while($result = mysql_fetch_array($results)){
 	$amount = $result['total'];
 	$btcaccount = $result['userid'].'-'.$result['id'];
-	$received = $litecoin->getbalance($btcaccount);
+	$received = $zenzo->getbalance($btcaccount);
 	//print($received);
 	if($amount <= $received){
 		//echo 'PAID';
 		$fee = 0;
-		$transid = $litecoin->getaccountaddress($btcaccount.'-'.$result['id']);
+		$transid = $zenzo->getaccountaddress($btcaccount.'-'.$result['id']);
 		//checkCbTransID($transid); 
 		addInvoicePayment($result['id'],$transid,$received,$fee,$gatewaymodule);
 		logTransaction($GATEWAY["name"],array('address'=>$transid,'amount'=>$received),"Successful");
